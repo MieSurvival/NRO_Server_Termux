902f55b04c545a524b603b23777037f0b1461780
MieSurvival <127181306+MieSurvival@users.noreply.github.com>
2023-03-11 08:37:24 +0700

    Update index.php

--- khanh/index.php
+++ khanh/index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "NinjaSchoolOnTermux\r\n";
+echo "NroServerTermux\r\n";
 echo "<br>";
-echo nl2br("\n Nguyễn Văn Khánh (KhanhNguyen9872) \n Github: https://github.com/khanhnguyen9872 \r\n Facebook: https://fb.me/khanh10a1");
+echo nl2br("\n Mie Survival (Mie) \n Github: https://github.com/miesurvival \r\n Facebook: có cái nịt");
 
 ?>
